fbfdfdf67fd46749b265552df8ef78dce7667b80
Wh4tz <151379163+Nu-man-71@users.noreply.github.com>
2025-09-14 21:29:02 +0545

    Add files via upload

---
+++ qrcodegenerator.py
@@ -0,0 +1,13 @@
+import qrcode
+
+# Create QR code
+def generate_qr(data, filename="qrcode.png"):
+    qr = qrcode.QRCode(version=1, box_size=10, border=5)
+    qr.add_data(data)
+    qr.make(fit=True)
+    
+    img = qr.make_image(fill_color="black", back_color="white")
+    img.save(filename)
+    print(f"QR code saved as {filename}")
+
+# pip install qrcode[pill ] is neccesary for qr 
